9bf11d6cf8b23e89445d30b427e3a5d8edc0b126
Igor Korshuk <korshuk@lyceum.by>
2014-02-08 17:03:53 +0300

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,57 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'lyceum.by'
+set :repo_url, 'git@github.com:korshuk/lyceum.by.git'
+set :branch, 'master'
+set :user, 'lyceum'
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/www/lyceum'
+set :scm, :git
+
+set :format, :pretty
+
+set :log_level, :debug
+
+set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+set :keep_releases, 5
+
+namespace :deploy do
+#TODO: Add stop task in upstart
+  desc "Stop Forever"
+  task :started do
+    on roles(:app) do
+      execute "forever stopall" 
+    end
+  end
+ 
+  desc "Install node modules non-globally"
+  task :npm_install do
+    on roles(:app) do
+      execute "cd #{current_path} && npm install"
+    end
+  end
+ 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # This assumes you are using upstart to startup your application 
+      # - be sure that your upstart script runs as the 'deploy' user
+      execute "sudo start node-upstart-script", raise_on_non_zero_exit: false
+    end
+  end
+ 
+  before :restart, 'deploy:npm_install'
+end
