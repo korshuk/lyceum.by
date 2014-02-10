5a89aba3600a1b54a5774433764954d90143f023
Igor Korshuk <korshuk@lyceum.by>
2014-02-10 13:12:02 +0300

    public images

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,15 +59,13 @@ namespace :deploy do
     end
   end
 
-  desc <<-DESC
-    Creates symbolic links to configuration files and other dependencies
-    after deployment.
-  DESC
+  desc 'Creates symbolic links to configuration files and other dependencies after deployment.'
   task :link_dependencies do
-    run "mkdir -p /public/images/desktop && chmod g+w /public/images/desktop"
-    run "ln -nfs #{shared_path}/public/images #{release_path}/public/images"
-    run "mkdir -p /public/images/mobile && chmod g+w /public/images/mobile"
-    run "ln -nfs #{shared_path}/public/images #{release_path}/public/images"
+    on roles(:app), in: :sequence, wait: 5 do
+    execute "mkdir -p /public/images/desktop && chmod g+w /public/images/desktop"
+    execute "ln -nfs #{shared_path}/public/images #{release_path}/public/images"
+    execute "mkdir -p /public/images/mobile && chmod g+w /public/images/mobile"
+    execute "ln -nfs #{shared_path}/public/images #{release_path}/public/images"
   end
   
   after :deploy, 'deploy:link_dependencies'
