fdf9636fbb2c19dbcc0136ecf647e303c1283a74
Igor Korshuk <korshuk@lyceum.by>
2019-05-05 21:30:23 +0300

    cap config fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,9 +10,9 @@ set :stage, :production
 # role :web, %w{lyceum@178.159.244.43}
 # role :db,  %w{lyceum@178.159.244.43}
 
-role :app, %w{lyceum@185.179.83.146}
-role :web, %w{lyceum@185.179.83.146}
-role :db,  %w{lyceum@185.179.83.146}
+role :app, %w{root@185.179.83.146}
+role :web, %w{root@185.179.83.146}
+role :db,  %w{root@185.179.83.146}
 
 # Extended Server Syntax
 # ======================
@@ -21,7 +21,7 @@ role :db,  %w{lyceum@185.179.83.146}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 # server '178.159.244.43', user: 'lyceum', roles: %w{web app}, my_property: :my_value
-server '185.179.83.146', user: 'lyceum', roles: %w{web app}, my_property: :my_value
+server '185.179.83.146', user: 'root', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -35,7 +35,7 @@ server '185.179.83.146', user: 'lyceum', roles: %w{web app}, my_property: :my_va
 # and/or per server
 # server '178.159.244.43',
  server '185.179.83.146', 
-  user: 'lyceum',
+  user: 'root',
    roles: %w{web app},
    ssh_options: {
 #     user: 'user_name', # overrides user setting above
