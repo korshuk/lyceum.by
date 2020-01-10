ca1ee1f3da2d92b9c9eb26282fffb5aa0095f374
Diana <58979277+DianaRozova@users.noreply.github.com>
2020-01-10 11:08:50 +0300

    Add files via upload

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -29,7 +29,10 @@ ContactsController = function(mongoose, application) {
       doc.phone = req.body['phone'];
       doc.email = req.body['email'];
       doc.place = req.body['place'];
-      doc.calendarapi = req.body['calendarapi'];
+      doc.client_id = req.body['client_id'];
+      doc.client_secret = req.body['client_secret'];
+      doc.redirect_uris = req.body['redirect_uris'];
+
       doc.order = req.body['order'] || 0;
       
       doc.save(function(err) {
@@ -57,6 +60,7 @@ ContactsController = function(mongoose, application) {
       } 
       else {
         req.session.success = 'Контакт <strong>' + doc.name.ru + '</strong> создан ' + doc.createdAt;
+        if (doc.token != '') upload.single('token');
         res.redirect(self.path);
       }
     });
@@ -81,6 +85,94 @@ ContactsController = function(mongoose, application) {
       res.render('makeAppointment',{doc: doc});
     });
   }
+
+
+  base.postAppointmentForm = function(req, res){
+    this.Collection.findByReq(req,res,function(doc){
+      var endDate = new Date(req.body.date + " " + req.body.time);
+      endDate.setMinutes(endDate.getMinutes()+20);
+      var client = {
+        'id': doc._id,
+        'startdate': new Date(req.body.date + " " + req.body.time),
+        'enddate': endDate,
+        'clientemail': req.body.email,
+        'discription': 'Встреча с ' + req.body.fullName + ', телефон: ' + req.body.phone,
+
+        'email': doc.email,
+        'client_secret': doc.client_secret,
+        'client_id': doc.client_id,
+        'redirect_uris': doc.redirect_uris
+      }
+      createEvent(client);
+      res.render('makeAppointment',{doc: doc});
+    });
+  }
+
+  createEvent = async function(client){
+    var event = {
+      'summary': client.discription,
+      'description': client.discription,
+      'start': {
+        'dateTime': client.startdate
+      },
+      'end': {
+        'dateTime': client.enddate
+      },
+      'attendees':[
+        {'email': client.clientemail},
+        {'email': client.email}
+      ]
+    }
+
+    var fs = require('fs');
+    var readline = require('readline');
+    var {google} = require('googleapis');
+    var SCOPES = ['https://www.googleapis.com/auth/calendar'];
+    var TOKEN_PATH = client.id+'.json';
+    authorize(client, newEvent);
+    function authorize(client, callback) {
+      // var {client_secret, client_id, redirect_uris} = credentials.installed;
+      var oAuth2Client = new google.auth.OAuth2(
+        client.client_id, client.client_secret, client.redirect_uris);
+      fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) return getAccessToken(oAuth2Client, callback);
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+      });
+    }
+    function getAccessToken(oAuth2Client, callback) {
+      var authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+      });
+      console.log('Authorize this app by visiting this url:', authUrl);
+      var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+          if (err) return console.error('Error retrieving access token', err);
+          oAuth2Client.setCredentials(token);
+          fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) return console.error(err);
+            console.log('Token stored to', TOKEN_PATH);
+          });
+          callback(oAuth2Client);
+        });
+      });
+    }
+    function newEvent(auth) {
+      var calendar = google.calendar({version: 'v3', auth});
+      calendar.events.insert({
+        'calendarId': 'primary',
+        'resource': event
+      }, function() {
+        console.log('Event created');
+      });
+    }
+  };
 
   base.constructor = arguments.callee;
 
